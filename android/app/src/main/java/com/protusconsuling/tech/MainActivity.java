9d6874990341e1a9532db8152af6e8baef07b7dd
OdidaProtas <bryodiiidah@gmail.com>
2022-02-14 16:29:19 +0300

    setup

---
+++ android/app/src/main/java/com/protusconsuling/tech/MainActivity.java
@@ -0,0 +1,5 @@
+package com.protusconsuling.tech;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ build_installer.js
@@ -0,0 +1,38 @@
+// ./build_installer.js
+
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64", 
+const APP_DIR = path.resolve(__dirname, './release-builds/DreamerApp-win32-ia32');
+// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer", 
+const OUT_DIR = path.resolve(__dirname, './windows_installer');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'DREAM Information managment systems. Efficiency matters.',
+    exe: 'DreamerMIS',
+    name: 'Dream MIS',
+    manufacturer: 'Protus Consulting',
+    version: '1.0.0',
+
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true
+    },
+    appIconPath: path.resolve(__dirname, './favicon.ico'),
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function() {
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
